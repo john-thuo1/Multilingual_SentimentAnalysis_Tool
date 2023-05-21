4fafe1c0c358db3e13a1d10fec04dd280fbf8002
john-thuo1 <108690517+john-thuo1@users.noreply.github.com>
2023-05-21 16:23:14 +0300

    Proj

--- sentiment.py
+++ sentiment.py
@@ -8,6 +8,7 @@ import torch
 import matplotlib.pyplot as plt
 
 # Load the NLP Model from Hugging Face
+@st.cache_data()
 def load_model():
     tokenizer = AutoTokenizer.from_pretrained('nlptown/bert-base-multilingual-uncased-sentiment')
     model = AutoModelForSequenceClassification.from_pretrained('nlptown/bert-base-multilingual-uncased-sentiment')
@@ -19,19 +20,6 @@ def sentiment_score(review, tokenizer, model):
     result = model(tokens)
     return int(torch.argmax(result.logits) + 1)
 
-def generate_sentiment_boxplot(df):
-    # Create the box plot
-    fig, ax = plt.subplots(figsize=(10, 6))
-    ax.boxplot(df.groupby('Month')['Sentiment Score'].apply(list).values)
-    ax.set_title('Sentiment Scores by Month')
-    ax.set_xlabel('Month')
-    ax.set_ylabel('Sentiment Score')
-
-    # Set x-axis tick labels
-    ax.set_xticks(range(1, len(df['Month'].unique()) + 1))
-    ax.set_xticklabels(df['Month'].unique())
-
-    return fig
 
 def main():
     st.title("Multilingual Sentiment Analysis Tool For Your Business")
@@ -41,7 +29,7 @@ def main():
         df = pd.read_csv(csv_file)
         df['Sentiment Score'] = 0  # Add the 'Sentiment Score' column with default values
         snapshot = df.head(3)
-        st.dataframe(snapshot)  # Display the first 3 rows
+        st.dataframe(snapshot) 
 
         # Load the model
         tokenizer, model = load_model()
@@ -49,7 +37,11 @@ def main():
         # Press this button to see results
         if st.button("Analyze"):
             # Perform sentiment analysis on the 'Review' column
-            df['Sentiment Score'] = df['Review'].apply(lambda x: sentiment_score(x, tokenizer, model))
+            df['Sentiment Score'] = df['Review'].apply(lambda x: sentiment_score(x[:512], tokenizer, model) if pd.notna(x) else 0)
+
+            # Map sentiment scores to overall sentiment categories
+            sentiment_mapping = {5: 'Positive', 4: 'Positive', 3: 'Neutral', 1: 'Negative', 2: 'Negative'}
+            df['Overall'] = df['Sentiment Score'].map(sentiment_mapping)
 
             # Display the updated DataFrame
             st.dataframe(df.head())
@@ -63,16 +55,7 @@ def main():
                 file_name=csv_file_name,
                 mime='text/csv'
             )
-
-        # Option to generate graphs
-        if st.button("Generate Graphs"):
-            # Ensure the 'Sentiment Score' column is present
-            if 'Sentiment Score' not in df.columns:
-                st.write("Please analyze the reviews first to generate sentiment scores.")
-            else:
-                # Generate and display graphs here
-                fig = generate_sentiment_boxplot(df)
-                st.pyplot(fig)
+  
 
 if __name__ == '__main__':
     main()
