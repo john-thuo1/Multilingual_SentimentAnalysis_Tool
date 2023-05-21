685318d7d6a15e6adba011ab73dbc82b59a27977
john-thuo1 <108690517+john-thuo1@users.noreply.github.com>
2023-05-21 21:02:22 +0300

    changed openai engine

--- pages/recommendation.py
+++ pages/recommendation.py
@@ -4,42 +4,42 @@ import os
 from dotenv import load_dotenv
 import openai
 
-# loading env variables
+# Loading env variables
 load_dotenv()
-# OpenAI API endpoint
+print(os.getenv('OPENAI_API_KEY'))
 
-API_KEY = os.getenv('OPENAI_API_KEY')
-API_ENDPOINT = "https://api.openai.com/v1/engines/davinci/completions"
+def truncate_text(text, max_length):
+    if len(text) > max_length:
+        return text[:max_length] + "..."
+    return text
 
-
-
-def generate_recommendations(business_data):
-    recommendations = []
+def generate_recommendation(business_data, api_key):
+    # Generate prompt for ChatGPT based on all reviews and sentiment scores
+    prompt = ""
     for index, row in business_data.iterrows():
-        # Generate prompt for ChatGPT based on review and sentiment score
-        prompt = f"Review: {row['Review']}\nSentiment Score: {row['Sentiment Score']}\nRecommendation:"
-        print(f"Generating recommendation for index {index+1}...")
+        review = row['Review']
+        sentiment_score = row['Sentiment Score']
+        # Truncate the review if it exceeds a certain length
+        review = truncate_text(review, 500)
+        prompt += f"Review: {review}\nSentiment Score: {sentiment_score}\n"
 
-        # Generate completion using OpenAI API
-        response = openai.Completion.create(
-            engine="davinci",
-            prompt=prompt,
-            max_tokens=50  # Adjust the number of tokens for the desired response length
-        )
+    # Truncate prompt to a maximum length of 4096 tokens
+    prompt = truncate_text(prompt, 4096)
 
-        if response.choices[0].finish_reason == "stop":
-            completion = response.choices[0].text.strip()
-            recommendations.append(completion)
-        else:
-            recommendations.append("Error generating recommendation")
+    # Generate completion using OpenAI API
+    openai.api_key = api_key
+    response = openai.Completion.create(
+        engine="text-davinci-003",
+        prompt=prompt + "\nProvide a recommendation for the business based on the uploaded data:",
+        max_tokens=100,  # Increased the max_tokens value for a longer response
+    )
 
-        print(f"Recommendation generated for index {index+1}.")
+    if response.choices[0].finish_reason == "stop":
+        recommendation = response.choices[0].text.strip()
+    else:
+        recommendation = "Error generating recommendation"
 
-        # Limit the number of recommendations to 10
-        if len(recommendations) >= 10:
-            break
-
-    return recommendations[:10]
+    return recommendation
 
 
 def main():
@@ -59,17 +59,15 @@ def main():
         required_columns = ["Review", "Sentiment Score", "Date", "Month", "Year"]
         if set(required_columns).issubset(business_data.columns):
             st.subheader("Business Recommendation")
-            recommendations = generate_recommendations(business_data)
+            api_key = os.getenv('OPENAI_API_KEY')
+            recommendation = generate_recommendation(business_data, api_key)
 
             # Display recommendation
-            for index, recommendation in enumerate(recommendations):
-                st.write(f"Recommendation {index+1}:")
-                st.write(recommendation)
-                st.write("---")
+            st.write("Summary Recommendation:")
+            st.write(str(recommendation))  # Convert recommendation to string
         else:
             st.write("Error: The uploaded CSV file does not contain all the required columns.")
 
 
 if __name__ == "__main__":
-    openai.api_key = API_KEY
     main()
